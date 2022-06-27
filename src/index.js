854f4951d3797dcf57e54479bf840a334d91ad0b
Lyfthrasyr <dmitry.vorobyov91@gmail.com>
2022-06-27 22:54:58 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+	let arr = '' + n;
+	let newArr = arr.split('');
+	if (newArr[0] == '-') {
+		newArr.shift();
+	}
+
+	let newArray = newArr.reverse();
+	let newString = newArray.join('');
+
+	return parseInt(newString);
+
+
 }
